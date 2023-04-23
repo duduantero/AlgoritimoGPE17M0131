1bfcd7343d8adf85f4f6a0a2efa556c32a5d8544
Eduardo Antero de Moraes <96095997+duduantero@users.noreply.github.com>
2023-04-22 21:29:07 -0300

    update ex6 and ex7 in ListaAvaliativaPart1

--- ListaAvaliativaParte1/ex6.c
+++ ListaAvaliativaParte1/ex6.c
@@ -5,9 +5,25 @@
 int main (void)
 {
     setlocale(LC_ALL, "");
+    int menor = 1,maior = 0;
+    int vetor[10];
 
+    for (int x = 0; x < 10; x++)
+    {
+        printf("Informe o %iº: \n",x + 1);
+        scanf("%i",&vetor[x]);
+        if (vetor[x] < menor)
+        {
+            menor = vetor[x];
+        }
 
-
+        if (vetor[x] > maior)
+        {
+            maior = vetor[x];
+        }
+    }
+    printf("O menor número é %i \n",menor);
+    printf("O maior número é %i \n",maior);
     getch();
     return 0;
-    }
+}
--- ListaAvaliativaParte1/ex7.c
+++ ListaAvaliativaParte1/ex7.c
@@ -5,9 +5,21 @@
 int main (void)
 {
     setlocale(LC_ALL, "");
+    int maior = 0,posicao;
+    int vetor[10];
 
-
-
+    for (int x = 0; x < 10; x++)
+    {
+        printf("Informe o %iº: \n",x + 1);
+        scanf("%i",&vetor[x]);
+        if (vetor[x] > maior)
+        {
+            maior = vetor[x];
+            posicao = x;
+        }
+    }
+    printf("Posição do vetor é %i ou vetor[%i] \n",posicao,posicao);
+    printf("O maior número é o %i \n",maior);
     getch();
     return 0;
-    }
+}
